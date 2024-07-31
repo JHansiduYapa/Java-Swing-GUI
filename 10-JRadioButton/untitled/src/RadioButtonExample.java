15368b0cdcbf3852250f64b72937c79eb96c43d3
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-07-31 09:22:42 +0530

    renamed files

---
+++ 10-JRadioButton/untitled/src/RadioButtonExample.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class RadioButtonExample {
+    // create a frame
+    JFrame frame;
+    JRadioButton male,female;
+    JButton btn;
+
+    public RadioButtonExample(){
+        // make a frame to add
+        frame =new JFrame();
+
+        // make radio button
+        male = new JRadioButton("Male");
+        female = new JRadioButton("Female");
+
+        // make 
+    }
+
+}
